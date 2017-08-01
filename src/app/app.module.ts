bf7d9b37c07c473579e0cc1ab66a13ae32872f28
wengdezhao <wengdezhao@maka.im>
2017-08-01 15:36:47 +0800

    set plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { StarsComponent } from './stars/stars.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent,
+    StarsComponent,
+    SearchComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
